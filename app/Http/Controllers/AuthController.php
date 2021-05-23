7776c5c7aa63cdd0e0d081e2c16cb13cc8aa4459
Justin <justintrang01@gmail.com>
2021-05-24 06:01:49 +1200

    add Sanctum & protect API access

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Invalid login details'
+            ], 401);
+        }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function logout()
+    {
+        // Get user who requested the logout
+        return Auth::user()->currentAccessToken()->delete();
+    }
+
+    public function me(Request $request)
+    {
+        return $request->user();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,8 +15,11 @@ use App\Http\Controllers\StudentController;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
-Route::post('/student', [StudentController::class, 'store']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    Route::get('/logout', [AuthController::class, 'logout']);
+    Route::post('/student', [StudentController::class, 'store']);
+    Route::post('/me', [AuthController::class, 'me']);
+});
